31f9dcf7d663a49e7db75f797efcd821bd9309a0
Fabio Petrillo <petrillo@bitbucket.org>
2015-10-14 12:06:04 -0400

    Cleaning imports and original comments.

--- src/test/java/swarm/server/JpaApplicationTests.java
+++ src/test/java/swarm/server/JpaApplicationTests.java
@@ -1,19 +1,13 @@
 package swarm.server;
 
-import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.lang.management.ManagementFactory;
-
-import javax.management.ObjectName;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -21,20 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-/**
- * Integration test to run the application.
- *
- * @author Oliver Gierke
- * @author Dave Syer
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-// Enable JMX so we can test the MBeans (you can't do this in a properties file)
-@TestPropertySource(properties = { "spring.jmx.enabled:true",
-		"spring.datasource.jmx-enabled:true" })
-@ActiveProfiles("swarm")
-// Separate profile for web tests to avoid clashing databases
+@TestPropertySource(properties = { "spring.jmx.enabled:true", "spring.datasource.jmx-enabled:true" })
 public class JpaApplicationTests {
 
 	@Autowired
